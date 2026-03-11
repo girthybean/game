9069942f2f28a269c21fe4302c9199f0d911eb54
Vitalii Kocherha <vitalii.kocherha@appsflyer.com>
2026-03-11 08:37:19 +0200

    Basic REST endpoints

--- src/main/java/com/appsflyer/game/controller/GameController.java
+++ src/main/java/com/appsflyer/game/controller/GameController.java
@@ -4,6 +4,9 @@ import com.appsflyer.game.repository.GameRepository;
 import com.appsflyer.game.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,25 +18,24 @@ public class GameController {
     private final TeamRepository teamRepository;
     private final GameRepository gameRepository;
 
-
+    @PostMapping("team")
     public ResponseEntity<?> registerTeam() {
         return ResponseEntity.ok("stub");
     }
 
+    @PostMapping("answer")
     public ResponseEntity<?> registerAnswer() {
         return ResponseEntity.ok("stub");
     }
 
-    public ResponseEntity<?> teamStats() {
-        return ResponseEntity.ok("stub");
+    @GetMapping("team/{team}")
+    public ResponseEntity<?> teamStats(@PathVariable String team) {
+        return ResponseEntity.ok(team);
     }
 
+    @GetMapping("total")
     public ResponseEntity<?> gameTotal() {
         return ResponseEntity.ok("stub");
     }
-
-
-
-
 
 }
