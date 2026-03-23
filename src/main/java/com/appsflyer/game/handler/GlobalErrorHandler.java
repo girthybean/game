dc3edd4db2927d160f9af86fb19a5a9171b9d072
Vitalii Kocherha <vitalii.dev@pm.me>
2026-03-23 10:31:49 +0200

    handle not found

--- src/main/java/com/appsflyer/game/handler/GlobalErrorHandler.java
+++ src/main/java/com/appsflyer/game/handler/GlobalErrorHandler.java
@@ -1,6 +1,7 @@
 package com.appsflyer.game.handler;
 
 import com.appsflyer.game.exceptions.WrongAnswerException;
+import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,4 +14,10 @@ public class GlobalErrorHandler {
     ResponseEntity<?> handleWrongAnswer(WrongAnswerException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
+
+    @ExceptionHandler(exception = NoSuchElementException.class)
+    ResponseEntity<?> handleNotFound(NoSuchElementException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
 }
